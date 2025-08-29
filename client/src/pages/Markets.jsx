6684576ea51f8f6550a7180b0bc09d191c545c01
kiptalam1 <adamskiptalam0@gmail.com>
2025-08-29 12:25:54 +0300

    feat: Display markets to normal users.

--- client/src/pages/Markets.jsx
+++ client/src/pages/Markets.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import useFetch from "../hooks/useFetch";
 import AdminMarkets from "../components/markets/AdminMarkets";
 import { useAuth } from "../contexts/AuthContext";
+import UserMarkets from "../components/markets/UserMarkets";
 
 const Markets = () => {
 	const { loading: authLoading, user } = useAuth();
@@ -13,13 +14,15 @@ const Markets = () => {
 		refetch,
 	} = useFetch("/api/markets/");
 
-	// if (authLoading) {
-	// 	return <p className="text-center">loading...</p>;
-	// }
+	const isAdmin = user?.role === "admin";
 
 	return (
 		<div className="min-h-screen w-full bg-[var(--color-bg)] text-[var(--color-text)] p-4 sm:p-6 sm:px-10 md:px-16 lg:px-20 flex flex-col items-center gap-3 font-[var(--font-sans)]">
-			{user && (
+			{authLoading ||
+				(fetchMarketsLoading && (
+					<div className="w-4 h-4 border-2 border-white border-dashed rounded-full animate-spin"></div>
+				))}
+			{user && isAdmin && (
 				<>
 					<AdminMarkets
 						data={data}
@@ -29,10 +32,16 @@ const Markets = () => {
 					/>
 				</>
 			)}
-			{authLoading ||
-				(fetchMarketsLoading && (
-					<div className="w-4 h-4 border-2 border-white border-dashed rounded-full animate-spin"></div>
-				))}
+			{user && !isAdmin && (
+				<>
+					<UserMarkets
+						data={data}
+						error={error}
+						loading={fetchMarketsLoading}
+						refetch={refetch}
+					/>
+				</>
+			)}
 		</div>
 	);
 };
